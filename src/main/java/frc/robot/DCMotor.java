6da2ff7a09b6aeeafd87769b8b19b945b6814336
KCSquid <jahvonc9@gmail.com>
2025-02-20 15:32:32 -0500

    new dcmotor functions and dpad mappings

--- src/main/java/frc/robot/DCMotor.java
+++ src/main/java/frc/robot/DCMotor.java
@@ -5,6 +5,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class DCMotor extends SubsystemBase {
   private final TalonFX motor;
+  private static final double DEFAULT_SPEED = 0.25;
 
   public DCMotor(int motorID) {
     motor = new TalonFX(motorID);
@@ -16,5 +17,23 @@ public class DCMotor extends SubsystemBase {
 
   public void stop() {
     motor.set(0);
+  }
+
+  public void addSpeed() {
+    double speed = motor.get();
+    if (speed >= 1) {
+      return;
+    }
+
+    motor.set(speed + DEFAULT_SPEED);
+  }
+
+  public void subtractSpeed() {
+    double speed = motor.get();
+    if (speed <= -1) {
+      return;
+    }
+
+    motor.set(speed - DEFAULT_SPEED);
   }
 }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -30,16 +30,17 @@ public class RobotContainer {
   
   private final SwerveSubsystem swerveSubsystem = new SwerveSubsystem();
   private final HookSubsystem hook = new HookSubsystem(Constants.Hook.leftHook, Constants.Hook.rightHook);
-  private final IntakeSubsystem intake = new IntakeSubsystem(Constants.Intake.m_1ID, Constants.Intake.m_1Inverted, Constants.Intake.m_2ID, Constants.Intake.m_2Inverted);
+  // private final IntakeSubsystem intake = new IntakeSubsystem(Constants.Intake.m_1ID, Constants.Intake.m_1Inverted, Constants.Intake.m_2ID, Constants.Intake.m_2Inverted);
   private final ShooterSubsystem shooter = new ShooterSubsystem(Constants.Shooter.mID, Constants.Shooter.mInverted);
   private final PhotonVision photonVision = new PhotonVision("1");
   
   // * declare motor
-  private final DCMotor dcMotor = new DCMotor(1); // TODO: add actual ID (1)
-  private final int dcMotorSpeed = 0.5
+  private final DCMotor dcMotor = new DCMotor(Constants.DCMotor.motorID); // TODO: add actual ID (1)
+  private final double dcMotorSpeed = Constants.DCMotor.motorSpeed;
 
   private final XboxController xbox = new XboxController(0);
   private final Joystick joystick = new Joystick(0);
+  private final Dpad dpad = new Dpad(joystick);
 
 
   public RobotContainer() {
@@ -62,10 +63,18 @@ public class RobotContainer {
 
   private void configureBindings() {
 
-    // * control motor with xbox controller (control with 1 and 2 to move and stop)
-    // TODO: change button IDs if needed (buttons 1 and 2)
+    // ! old way of controlling motor. sets speed to 0.5 when button 1 is pressed, and -0.5 when button 2 is pressed
+    new POVButton(joystick, 0).onTrue(new InstantCommand(() -> dcMotor.setSpeed(dcMotorSpeed)));
+    new POVButton(joystick, 180).onTrue(new InstantCommand(() -> dcMotor.setSpeed(-dcMotorSpeed)));
+
+    // * new way. adds 0.25 to speed or subtracts 0.25 from speed when button 1 or 2 is pressed, respectively
+    // new POVButton(joystick, 0).onTrue(new InstantCommand(() -> dcMotor.addSpeed()));
+    // new POVButton(joystick, 180).onTrue(new InstantCommand(() -> dcMotor.subtractSpeed()));
+    // new POVButton(joystick, 270).onTrue(new InstantCommand(() -> dcMotor.stop()));
+
     new JoystickButton(xbox, 1).whileTrue(new InstantCommand(() -> dcMotor.setSpeed(dcMotorSpeed)));
     new JoystickButton(xbox, 2).whileTrue(new InstantCommand(() -> dcMotor.stop()));
+    new JoystickButton(xbox, 3).whileTrue(new InstantCommand(() -> dcMotor.setSpeed(-dcMotorSpeed)));
 
     new JoystickButton(xbox, 4).onTrue(new InstantCommand(() -> swerveSubsystem.zeroHeading()));
     new POVButton(joystick, 0).whileTrue(new HookDPAD(hook, true));
